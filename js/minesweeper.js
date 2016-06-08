45b90a519a3eace8e5a516999c7266b89fe31dce
mjgwood <matthewjgwood@gmail.com>
2016-06-08 16:22:35 +0800

    add table and app layout

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,11 +1,58 @@
 var minesweeper = (function() {
+  var squares = [],
+      mines = [];
 
-  createGrid: function() {
-    
-  }
+  // Grid module
+  var grid = {
+
+    // Create grid according to numRows x numCols and fill squares array
+    createGrid: function() {
+      var numRows = 9,
+          numCols = 9,
+          toAdd = "";
+
+      for ( var i = 0; i < numRows; i++ ) {
+        toAdd += "<tr>";
+
+        for ( var j = 0; j < numCols; j++ ) {
+          squares.push( [i,j] );
+          toAdd += "<td id=" + (i * numRows + j) + "></td>";
+        }
+
+        toAdd += "</tr>";
+      }
+
+      $("tbody").empty().append(toAdd);
+      // loadMines();
+      // displayTimer();
+    },
+
+    loadMines: function() {
+      for ( var i = 0; i < 9; i ++ ) {
+        col = Math.floor(Math.random() * (9 + 1));
+        row = Math.floor(Math.random() * (9 + 1));
+        mines.push([col,row]);
+      }
+
+      displayMines();
+    },
+
+    displayMines: function() {
+      
+    },
+
+    // Take x & y values of a square and return square id
+    findSquareId: function(i,j) {
+
+    }
+  };
+
+  return {
+    grid: grid
+  };
 
 })();
 
 $(document).ready( function() {
-
+  minesweeper.grid.createGrid();
 });
