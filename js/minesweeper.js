b449e6815bc3e636274f6ea6c9d79c6fd8752a19
mjgwood <matthewjgwood@gmail.com>
2016-06-08 21:02:59 +0800

    add mines

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,9 +1,13 @@
 var minesweeper = (function() {
-  var squares = [],
-      mines = [];
 
   // Grid module
   var grid = {
+
+    inititialize: function() {
+      this.squares = [];
+      this.mines = [];
+      this.createGrid();
+    },
 
     // Create grid according to numRows x numCols and fill squares array
     createGrid: function() {
@@ -15,7 +19,7 @@ var minesweeper = (function() {
         toAdd += "<tr>";
 
         for ( var j = 0; j < numCols; j++ ) {
-          squares.push( [i,j] );
+          this.squares.push([i,j]);
           toAdd += "<td id=" + (i * numRows + j) + "></td>";
         }
 
@@ -23,27 +27,43 @@ var minesweeper = (function() {
       }
 
       $("tbody").empty().append(toAdd);
-      // loadMines();
+      this.loadMines();
       // displayTimer();
     },
 
+    // Generate unique random coords of mines, fill mines array, and call displayMines()
     loadMines: function() {
-      for ( var i = 0; i < 9; i ++ ) {
-        col = Math.floor(Math.random() * (9 + 1));
-        row = Math.floor(Math.random() * (9 + 1));
-        mines.push([col,row]);
+      var i = 0;
+
+      while ( i < 10 ) {
+        var row = Math.floor(Math.random() * 9);
+        var col = Math.floor(Math.random() * 9);
+        var dupeFound = false;
+
+        for ( var j = 0; j < this.mines.length; j++ ) {
+          if ( this.mines[j][0] === row && this.mines[j][1] === col ) {
+            dupeFound = true;
+          }
+        }
+
+        if ( dupeFound === false ) {
+          this.mines.push([row, col]);
+          i++;
+        }
       }
 
-      displayMines();
+      this.displayMines();
     },
 
     displayMines: function() {
-      
+      for ( var i = 0; i < this.mines.length; i++ ) {
+        $("#" + this.getSquareId(this.mines[i])).addClass("mine");
+      }
     },
 
     // Take x & y values of a square and return square id
-    findSquareId: function(i,j) {
-
+    getSquareId: function(array) {
+      return array[1] * 9 + array[0];
     }
   };
 
@@ -54,5 +74,5 @@ var minesweeper = (function() {
 })();
 
 $(document).ready( function() {
-  minesweeper.grid.createGrid();
+  minesweeper.grid.inititialize();
 });
